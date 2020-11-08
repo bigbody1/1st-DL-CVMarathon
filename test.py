0af35a71316012da8a2eb923ebf88cd60e042d64
bigbody1 <qqes7762@gmail.com>
2020-11-08 11:24:54 +0800

    D1

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import cv2
+img = cv2.imread('lena.png', cv2.IMREAD_COLOR)
+img2 = cv2.imread('lena.png', cv2.IMREAD_GRAYSCALE)
 
-cv2.__version__
+cv2.imshow('rgb', img)
+cv2.imshow('gray', img2)
+cv2.waitKey(0)
+
+
 
