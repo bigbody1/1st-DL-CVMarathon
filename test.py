9c43ec6a2c14815d1fef2ffc1a01f9f249c04e67
bigbody1 <qqes7762@gmail.com>
2020-11-08 11:46:36 +0800

    D1_2

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import cv2
+import numpy as np
+
 img = cv2.imread('lena.png', cv2.IMREAD_COLOR)
-img2 = cv2.imread('lena.png', cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow('rgb', img)
-cv2.imshow('gray', img2)
+img_1 = img[:,:,0]
+img_2 = img[:,:,1]
+img_3 = img[:,:,2]
+
+cv2.imshow('image1', img_1)
+cv2.imshow('image2', img_2)
+cv2.imshow('image3', img_3)
+
 cv2.waitKey(0)
-
-
 
